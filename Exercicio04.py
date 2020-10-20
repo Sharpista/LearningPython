0a004c573a410e46810c3b88154e2f192272da5a
Sharpista <alexandrerobertofilho@gmail.com>
2020-10-19 21:10:59 -0300

    adicionando arquivos python

---
+++ Exercicio04.py
@@ -0,0 +1,10 @@
+#Faça um Programa que leia um vetor de 10 caracteres, e diga quantas consoantes foram lidas. Imprima as consoantes.#
+
+
+arrayCaracteres = []
+
+for c in range(1, 11):
+    arrayCaracteres.append(input('Entre com o caractere :'))
+
+
+for caracteres in arrayCaracteres:
